efc425c05322ddf320d572e1113c294a50446bd0
Bishesh <bishesh.sunam@gmail.com>
2022-03-13 17:59:10 +1300

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import Navigation from "./components/Navigation.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
-  <Navigation/>
-  <RouterView />
+  <Navigation />
+  <main>
+ <RouterView />
+  </main>
+ 
+  <Footer />
 </template>
 
 <style>
 @import "@/assets/base.css";
+main { 
+  min-height: 100vh;
+}
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1>All right preserved &copy; bisheshsunam</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  height: 100px;
+  width: 100%;
+  background: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+h1 {
+    font-size: 18px;
+  color: white;
+  text-align: center;
+}
+</style>
